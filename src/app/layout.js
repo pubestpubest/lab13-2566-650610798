81fca525590c531bd16ca87b030ce1772863b5be
pubestpubest <pubestpubest@gmail.com>
2023-08-10 20:31:21 +0700

    1st commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Pubest Ruengkum"
+            studentId="650610798"
           />
         </div>
       </body>
--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,28 +3,40 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ params }) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
+  const processedSearchInput = params.searchInput.replaceAll("%20", " ");
 
   /*
   tip2 : Use "includes" string method to check substring
   Example : "ABC".includes("AB") -> return true
 
   tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
-  to convert movie title and searchInput to lower case 
+  to convert movie title and searchInput to lower case */
   const filteredMovies = movieDB.filter((movie) =>
-    you code here...
+    movie.title
+      .toLocaleLowerCase()
+      .includes(processedSearchInput.toLocaleLowerCase())
   );
-  */
 
+  console.log(params);
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          title={movie.title}
+          rating={movie.rating}
+          number={i + 1}
+        ></MovieRow>
+      ))}
     </div>
   );
 }
